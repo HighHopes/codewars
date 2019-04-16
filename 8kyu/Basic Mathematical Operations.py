4a16d6b57e1a1135014772dd9b710d1e11f7812a
HighHopes <46654970+HighHopes@users.noreply.github.com>
2019-04-16 18:28:58 +0300

    done

--- 8kyu/Basic Mathematical Operations.py
+++ 8kyu/Basic Mathematical Operations.py
@@ -12,11 +12,17 @@ basic_op('/', 49, 7)        # Output: 7"""
 
 
 def basic_op(operator, value1, value2):
-	if operator == '+':
-		return value1 + value2
-	elif operator == '-':
-		return value1 - value2
-	elif operator == '*':
-		return value1 * value2
-	elif operator == '/':
-		return value1 / value2
+    if operator == "+":
+        return value1 + value2
+    elif operator == "-":
+        return value1 - value2
+    elif operator == "*":
+        return value1 * value2
+    elif operator == "/":
+        return int(value1 / value2)
+
+
+print(basic_op('+', 4, 7))  # 11
+print(basic_op('-', 15, 18))  # -3
+print(basic_op('*', 5, 5))  # 25
+print(basic_op('/', 49, 7))  # 7
