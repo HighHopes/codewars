c545065603780bd928cd31801bd1602a03fde18b
HighHopes <46654970+HighHopes@users.noreply.github.com>
2019-06-06 15:42:35 +0300

    done

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
-r, g, b = 0, 0, 0
+def loves(name1, name2):
+    names = (name1 + name2).replace(" ", "").lower()
+    print(names)
+    
+    result = ""
+    temp = ""
+    
+    for i in "loves":
+        result += str(names.count(i))
+    
+    return result
 
-r = 0 if r <= 0 else 255 if r >= 255 else r
-g = 0 if g <= 0 else 255 if g >= 255 else g
-b = 0 if b <= 0 else 255 if b >= 255 else b
 
-result = hex(r)[-2:] + hex(g)[-2:] + hex(b)[-2:]
-
-print(result.upper())
-
-print(hex(0))
+print(loves("John Doe", "Jane Doe"))  # 66%
