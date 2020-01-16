37795490c7cfb0d326078e9bbf34c12db14e0f04
HighHopes <46654970+HighHopes@users.noreply.github.com>
2020-01-16 17:07:50 +0200

    -

--- 8kyu/How many lightsabers do you own.py
+++ 8kyu/How many lightsabers do you own.py
@@ -3,3 +3,7 @@ returns the number of lightsabers owned by that person.
 The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
 
 Note: your function should have a default parameter."""
+
+
+def how_many_light_sabers_do_you_own(name=""):
+    return 18 if name == "Zach" else 0
